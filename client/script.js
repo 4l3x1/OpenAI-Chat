853ff85430830ca2179856b1fed749402fdd603a
4l3x1 <alexishuarc@gmail.com>
2023-03-22 11:29:04 -0500

    Update URL server

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   // const response = await fetch("https://codex-im0y.onrender.com/", {
   // const response = await fetch("http://localhost:5000", {
-  const response = await fetch("https://openai-ka8v.onrender.com", {
+  const response = await fetch("https://openai-ka8v.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
